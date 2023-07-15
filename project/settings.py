147345d45f965ccf0bafe589483b440ec1da1da2
uzair8489 <62849686+uzair8489@users.noreply.github.com>
2023-07-15 15:42:21 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -137,7 +137,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'estore479@gmail.com'
-EMAIL_HOST_PASSWORD = 'estore123'
+EMAIL_HOST_PASSWORD = '********'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
